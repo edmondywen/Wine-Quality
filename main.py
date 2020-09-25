6b974a1519c03caf1a1fdc5fe9f17d60f0b80aa6
edmondywen <edmondywen@gmail.com>
2020-09-25 16:51:33 -0700

    Analyzing and organizing data using pandas

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import numpy as np
 import pandas as pd
 
+'reads a CSV file into DataFrame'
+DataFrame = pd.read_csv('/Users/edmond/PycharmProjects/Wine-Quality/data/winequality-red.csv', engine='python')
+DataFrame.head()
 
+'checks for null values including empty strings'
+pd.options.mode.use_inf_as_na = True
+DataFrame.isna()
 
+'provides info on the data and counts instances of each quality'
+DataFrame.info()
+print(DataFrame['quality'].value_counts())
+
+'changes target variable "quality" to Good or Bad, lists quantities of each'
+DataFrame['quality'] = ['Good' if x>=7 else 'bad' for x in DataFrame['quality']]
+print(DataFrame['quality'].value_counts())
